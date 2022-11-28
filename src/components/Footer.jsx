e9420430975bd67ccc560018e7b785556ad4dc86
Lilit Avanesyan <lilit_avanesyan_97@mail.ru>
2022-11-28 11:52:02 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,133 @@
+import {
+    Facebook,
+    Instagram,
+    Phone,
+    Room,
+    Twitter,MailOutline } from "@material-ui/icons";
+import styled from "styled-components";
+import React from 'react'
+import { mobile } from "../responsive";
+
+
+const Container = styled.div`
+display: flex;
+${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+display: flex;
+`;
+const SocialIcone = styled.div`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+color: white;
+background-color: #${(props) => props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+margin-right: 20px;
+`;
+
+const Center = styled.div`
+flex: 1;
+padding: 20px;
+${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+flex: 1;
+${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+
+const Footer = () => {
+  return (
+  <Container>
+     <Left>
+        <Logo>LiLiT</Logo>
+        <Desc>Online Shopping features are centralized in your admin,
+             so you can manage localization, shipping, duties,
+              and compliance from a single source.
+        </Desc>
+        <SocialContainer>
+            <SocialIcone color="3B5999">
+              <Facebook/>
+            </SocialIcone>
+            <SocialIcone color="E4405F">
+              <Instagram />
+            </SocialIcone>
+            <SocialIcone color="55ACEE">
+              <Twitter />
+            </SocialIcone>
+        </SocialContainer>
+     </Left>
+     <Center>
+     <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+     </Center>
+     <Right>
+     <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight:"10px"}}/> Azat Ankax Arcax
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight:"10px"}}/>+374 97 00 00 00
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight:"10px"}} />@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+    </Right>
+  </Container>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://kartinkin.net/uploads/posts/2021-07/1625842945_5-kartinkin-com-p-oboi-shopping-krasivie-5.jpg",
+      title: "Women's clothing",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    } ,
+    {
+      id: 2,
+      img: "https://www.kotak.com/content/dam/Kotak/product_card_images/how-credit-cards-enhance-your-shopping-experience.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    } ,
+    {
+      id: 3,
+      img: "https://www.kingfishershopping.co.uk/media/jjfpvc2b/shopping-banner.jpg?center=0.10413282349902948,0.7145827598123341&mode=crop&quality=100&width=780&height=600&rnd=132578992698100000",
+      title: "Women's blouse",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    } ,
+  ] ;
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://glamours.name/uploads/posts/2022-02/1644539425_3-glamours-name-p-zakupka-odezhdi-devushka-krasivo-foto-3.png",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://cdnn21.img.ria.ru/images/07e5/0b/0a/1758408620_0:0:3072:1728_1920x0_80_0_0_31c02d14dd7490840f8dccf9528dfb0a.jpg",
+      title: "Women's blouse",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/premium-photo/shopping-and-entertainment-mall-inside-two-beautiful-girls-with-paper-bags-at-the-mall-the-joy-of-consumption-gift-shopping-holiday_100894-1010.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://butik.am/public/gallery/items/large/42459.jpg",
+    },
+    {
+      id:2,
+      img:"https://koala.am/wp-content/uploads/2021/09/O1CN01ap7CUm1aEWjCDhQoR_0-item_pic-500x640.jpg",
+    },
+    {
+      id:3,
+      img:"https://koala.am/wp-content/uploads/2021/09/O1CN01JZD9EH22UqFe8MpBR_1938167124-500x640.jpg",
+    },
+    {
+      id:4,
+      img:"https://koala.am/wp-content/uploads/2021/09/O1CN01NXlDU61oDz1BeVk2M_712245192.jpg",
+    },
+    {
+      id:5,
+      img:"https://koala.am/wp-content/uploads/2021/04/O1CN01AEMcci28vlD9mAMI9_2639837995.jpg",
+    },
+    {
+      id:6,
+      img:"https://koala.am/wp-content/uploads/2020/09/O1CN01MVJHHm1GG5sJyxo6H_883880594.jpg",
+    },
+    {
+      id:7,
+      img:"https://forme.am/public/gallery/items/large/514477.jpg",
+    },
+    {
+      id:8,
+      img:"https://koala.am/wp-content/uploads/2020/09/O1CN01WhTiDl1GG5sPh5nlX_883880594.jpg",
+    },
+  ]
